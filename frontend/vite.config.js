0e7aaaeb2475c213706dfb61f768d739b71d648c
BaiganMarzhan008 <bajganmarzan11@gmail.com>
2026-05-03 22:04:44 +0500

    fix: pwa icon maskable

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -19,12 +19,14 @@ export default defineConfig({
           {
             src: '/letter-j.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: '/letter-j.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
